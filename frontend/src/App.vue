b6b63ec02873d143e3d012449286887a5507c1d0
Egzona <egzonarexhepi@u.boisestate.edu>
2019-09-14 18:39:11 -0400

    some changes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,12 +3,19 @@
     <v-app-bar app>
       <v-toolbar-title class="headline text-uppercase">
         <span>mathpix</span>
-        <span class="font-weight-light">snip</span>
+        <span class="font-weight-light">snip </span>
         <span>challenge</span>
       </v-toolbar-title>
+
+      <v-layout justify-end>
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+      </v-layout>
     </v-app-bar>
 
-    <v-content></v-content>
+    <v-content transition="slide-x-transition">
+      <router-view></router-view>
+    </v-content>
   </v-app>
 </template>
 
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,9 +1,24 @@
 <template>
-  <div>Hello</div>
+  <div class="home">
+     <v-jumbotron>
+      <v-container fill-height>
+        <v-layout justify-center>
+          <v-spacer></v-spacer>
+          <v-flex>
+            <h3 class="display-3">{{msg}}</h3>
+            <v-btn justify-center large color="primary" class="mx-0">Upload pdf</v-btn>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-jumbotron>
+  </div>
 </template>
 
 <script>
 export default {
-  components: {}
+  name: "App",
+  data: () => ({
+    msg: "Mathpix snip",
+  })
 };
 </script>
