174936d054a09099cd2b3015afdca01a5976a2d1
Yiwen Yu <yyu01@mica.edu>
2019-09-14 22:11:36 -0400

    added vue cards

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-     <v-jumbotron>
+    <v-jumbotron>
       <v-container fill-height>
         <v-layout justify-center>
           <v-spacer></v-spacer>
@@ -11,6 +11,35 @@
         </v-layout>
       </v-container>
     </v-jumbotron>
+
+    <v-container>
+      <v-row justify="center">
+        <v-col cols="6" md="4">
+          <v-card class="pa-2" outlined tile>
+            <v-img height="400" width="500" src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"></v-img>
+          </v-card>
+        </v-col>
+        <v-col cols="6" md="4">
+          <v-card class="pa-2" outlined tile>
+            <v-img height="400" width="500" src="https://cdn.vuetifyjs.com/images/cards/store.jpg"></v-img>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <v-card max-width="600" class="mx-auto">
+      <v-card-title>LaTeX</v-card-title>
+      <v-card-text>
+        <v-row no-gutters>
+          <template v-for="n in 4">
+            <v-col :key="n">
+              <v-card class="pa-4" outlined tile>Column</v-card>
+            </v-col>
+            <v-responsive v-if="n === 2" :key="`width-${n}`" width="100%"></v-responsive>
+          </template>
+        </v-row>
+      </v-card-text>
+    </v-card>
   </div>
 </template>
 
@@ -18,7 +47,8 @@
 export default {
   name: "App",
   data: () => ({
-    msg: "Mathpix snip",
+    media: true,
+    msg: "Mathpix snip"
   })
 };
 </script>
